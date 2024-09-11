988d91cbbd7893b93d7fc1afc48c06c6c3c85a0d
jonleed <124183777+jonleed@users.noreply.github.com>
2024-09-11 18:33:20 -0400

    Added EditContacts.php

---
+++ LAMPAPI/EditContacts.php
@@ -0,0 +1,41 @@
+<?php
+$inData = getRequestInfo();
+$newName = $inData["Name"];
+$newPhone = $inData["phone"];
+$newEmail = $inData["email"];
+$userId = $inData["userId"];
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    // "UPDATE Contacts SET (Name, Phone, Email) VALUES(?, ?, ?) WHERE UserID = ?)"
+    //$stmt = $conn->prepare("INSERT INTO Contacts (Name, Phone, Email, UserID) VALUES(?, ?, ?, ?)");
+    $stmt = $conn->prepare("UPDATE Contacts SET (Name, Phone, Email) VALUES(?, ?, ?) WHERE UserID = ?)");
+    $stmt->bind_param("sssi", $newName, $newPhone, $newEmail, $userId);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    returnWithInfo("Contact edited successfully");
+}
+
+function getRequestInfo() {
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function returnWithError($err) {
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($info) {
+    $retValue = '{"info":"' . $info . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function sendResultInfoAsJson($obj) {
+    header('Content-type: application/json');
+    echo $obj;
+}
+?>
