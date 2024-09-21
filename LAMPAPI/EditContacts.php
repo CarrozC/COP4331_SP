d1958ee8abcdc2bb0d051760b74f35276780367e
CarrozC <116290344+CarrozC@users.noreply.github.com>
2024-09-20 21:47:19 -0400

    Update EditContacts.php

--- LAMPAPI/EditContacts.php
+++ LAMPAPI/EditContacts.php
@@ -7,7 +7,7 @@ $newID = $inData["ID"];
 
 $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 
-if ($conn->connect_error) {-
+if ($conn->connect_error) {
     returnWithError($conn->connect_error);
 } else {
     $stmt = $conn->prepare("UPDATE Contacts SET Name = ?, Phone = ?, Email = ? WHERE ID = ?");
